bc46cd23649f8d9dcee4ffdb7829a8858e1949c3
Jonathan <informatica@tuagencia24.com>
2016-08-05 15:58:15 -0430

    Handler envio de email pagos

--- modules/RegistroDePagos/RegistroDePagosHandler.php
+++ modules/RegistroDePagos/RegistroDePagosHandler.php
@@ -1,4 +1,5 @@
 <?php
+//include('include/PHPMailer/enviar_email.php');
 class RegistroDePagosHandler extends VTEventHandler {
     function handleEvent($eventName, $entityData) {  
     	global $log, $adb;
@@ -9,6 +10,43 @@ class RegistroDePagosHandler extends VTEventHandler {
 				//$idp=$entityData->getId(); //OBTIENE ID DEL PAGO
 				//$idpago=$_REQUEST["record"];
 				//$this->updatePagos($idpago,$idVenta,NULL);
+				$host= $_SERVER["HTTP_HOST"];
+				$idpago		=$_REQUEST["record"];
+				$idVenta=$_REQUEST["registrodeventasid"];
+				$statuspago=$_REQUEST["pagostatus"];
+				$email="tuagencia.sistemas01@gmail.com";
+				$nombre="Hola,";
+				$asunto="Prueba CRM - Emitir SOTO (Pago Verificado)";
+				$mensaje = " 
+				<html>
+				<head> 
+				<title>Info - Tu Agencia 24</title> 
+				</head> 
+				<body> 
+				<p>".$nombre.",</p>
+				<p>Se ha verificado un nuevo pago para la Emisión de SOTO:</p>
+				<p><b>Pago: </b>".$idpago."</p>				
+				<p><b>Registro de Venta: </b> <a href='http://".$host."/index.php?module=RegistroDeVentas&view=Detail&record=".$idVenta."'>Click aqui</a></p>		
+				<BR><BR><BR>
+				<i>
+				Gracias,		
+				<p>Equipo TuAgencia24.com</p>
+				</i>
+				</body> 
+				</html> "; 
+				//Verificamos si es un SOTO
+				$sqlSoto="SELECT 1 FROM vtiger_localizadores WHERE registrodeventasid=? AND gds= ?";
+				$result = $adb->pquery($sqlSoto, array($idVenta,"Servi"));	
+				$row = $adb->fetch_row($result);
+				$esSoto=$row[0];
+
+				if ($statuspago=="Verificado" && $esSoto)				
+				$envio=enviarEmail($email,$asunto,$mensaje);			
+				if ($envio){
+					$sqlVentaUpdate="UPDATE vtiger_registrodeventas SET statussoto='Emitir Soto' WHERE registrodeventasid=?";
+					$result = $adb->pquery($sqlVentaUpdate, array($idVenta));	
+				}
+
         	}
     	}
     	return true;
