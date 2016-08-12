5959cecbb5a9c16960ff4b8c546921c7cd0788e0
Jonathan <informatica@tuagencia24.com>
2016-08-12 17:25:59 -0430

    Avances cron soto

--- modules/RegistroDeVentas/CronDatosSoto.php
+++ modules/RegistroDeVentas/CronDatosSoto.php
@@ -1,27 +1,49 @@
 <?php
 include("../../config.inc.php");
+include("../../include/PHPMailer/enviar_email.php");
 $user=$dbconfig['db_username'];
 $pass=$dbconfig['db_password'];
 $bd=$dbconfig['db_name'];
 mysql_connect("localhost",$user,$pass);
 mysql_select_db($bd);
 
-$sql="SELECT registrodeventasname FROM vtiger_registrodeventas WHERE registrodeventasid=?";
+$idinicio=17100; //LOCAL ESDE JUNIO
+$idinicio=21238; //PRODUCCION DESDE JULIO
+/*
+$sql="	SELECT vtiger_crmentity.crmid,vtiger_localizadores.localizador, vtiger_localizadores.contactoid,vtiger_localizadores.registrodeventasid,
+		FROM vtiger_localizadores 
+			INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = vtiger_localizadores.localizadoresid 
+			INNER JOIN vtiger_crmentityrel ON (vtiger_crmentityrel.relcrmid = vtiger_crmentity.crmid OR vtiger_crmentityrel.crmid = vtiger_crmentity.crmid)
+			LEFT JOIN vtiger_users ON vtiger_users.id = vtiger_crmentity.smownerid 
+			LEFT JOIN vtiger_groups ON vtiger_groups.groupid = vtiger_crmentity.smownerid  
+		WHERE vtiger_crmentity.deleted = 0 AND (vtiger_crmentityrel.crmid = 17186 OR vtiger_crmentityrel.relcrmid = 17186)";
+*/
+$sql="	SELECT e.crmid,r.registrodeventasname,e.smownerid				
+		FROM  vtiger_crmentity AS e
+		INNER JOIN vtiger_registrodeventas AS r ON r.registrodeventasid=e.crmid 				
+		WHERE r.registrodeventastype LIKE '%Boleto%' AND e.deleted = 0 AND e.crmid>21238 AND e.crmid NOT IN 
+		(SELECT vtiger_crmentityrel.crmid
+			FROM vtiger_crmentity
+			INNER JOIN vtiger_crmentityrel ON (vtiger_crmentityrel.relcrmid = vtiger_crmentity.crmid OR vtiger_crmentityrel.crmid = vtiger_crmentity.crmid)
+			WHERE vtiger_crmentity.deleted = 0 AND vtiger_crmentityrel.relmodule='Localizadores' AND e.crmid>21238 
+		)";
 
-$sql="SELECT 	vtiger_registrodeventas.registrodeventasname AS vtiger_registrodeventasregistrodeventasname,
-				vtiger_crmentity.smownerid AS vtiger_crmentityassigned_user_id,
-				vtiger_crmentity.createdtime AS vtiger_crmentitycreatedtime,
-				vtiger_crmentity.modifiedtime AS vtiger_crmentitymodifiedtime,
-				vtiger_crmentity.modifiedby AS vtiger_crmentitymodifiedby,
-				vtiger_registrodeventas.statussoto AS vtiger_registrodeventasstatussoto,
-				vtiger_crmentity.deleted 
-				FROM  vtiger_crmentity 
-				LEFT JOIN vtiger_registrodeventas ON vtiger_registrodeventas.registrodeventasid=vtiger_crmentity.crmid 				
-				WHERE  vtiger_crmentity.crmid=?  LIMIT 1";
+$result = mysql_query($sql);	
+$rows = mysql_num_rows($result);
+if ($rows>0){
+	while ($row=mysql_fetch_array($result)){
+		# code...
+		$id=$row["crmid"];
+		$venta=$row["registrodeventasname"];
+		$userid=$row["smownerid"];
+		$sqlUser="SELECT CONCAT(first_name,' ',last_name), email1 FROM vtiger_users WHERE id=".$userid;
+		$qryUser=mysql_query($sqlUser);
+		$rowUser=mysql_fetch_row($qryUser);
+		$user=$rowUser[0];
+		$email=$rowUser[1];
 
-$result = $adb->pquery($sql, array($crmid));	
-$row = $adb->fetch_row($result);
-$venta=$row[0];
+	}
+}
 
 $email="tuagencia.sistemas01@gmail.com";
 $nombre="Hola,";
@@ -43,11 +65,6 @@ Gracias,
 </i>
 </body> 
 </html> "; 
-//Verificamos si es un SOTO
-$sqlSoto="SELECT COUNT(*) FROM vtiger_localizadores WHERE localizadoresid=? AND gds= ?";
-$result = $adb->pquery($sqlSoto, array($relcrmid,"Servi"));	
-$row = $adb->fetch_row($result);
-$esSoto=$row[0];
 
 if ($esSoto)				
 $envio=enviarEmail($email,$asunto,$mensaje);			
